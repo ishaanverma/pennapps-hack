87570c586fadf8c1ccab5e5dabbde560b85e54f5
Ishaan Verma <ishaanverma97@gmail.com>
2021-09-11 01:01:21 -0700

    Added firebase service

---
+++ frontend/src/firebase.js
@@ -0,0 +1,52 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+} from "firebase/auth";
+import FirebaseContext from "./context/FirebaseContext";
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+class Firebase {
+  constructor() {
+    this.app = initializeApp(config);
+    this.auth = getAuth();
+  }
+
+  signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+
+    return signInWithPopup(this.auth, provider)
+      .then((result) => {
+        // const credential = GoogleAuthProvider.credentialFromResult(result);
+        // const token = credential.accessToken;
+        const user = result.user;
+
+        return user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+
+        console.log(`${errorCode} ${errorMessage} ${email} ${credential}`);
+      });
+  };
+
+  signOut = () => {
+    return signOut(this.auth);
+  };
+}
+
+export default Firebase;
+export { FirebaseContext };
